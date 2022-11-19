2768941e4c652b10998cf89c66293e1ebbb39143
Philipp Zagar <philipp.zagar@gmail.com>
2022-11-19 23:07:59 +0100

    .

--- client/src/components/GameOverPopUp/GameOverPopUp.tsx
+++ client/src/components/GameOverPopUp/GameOverPopUp.tsx
@@ -2,7 +2,7 @@ import "./GameOverPopUp.scss";
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { sendDataToDatabase } from "../../Scoreboard/serverapi.js";
-import { MDBTable } from 'mdb-react-ui-kit';
+import Table from 'rc-table';
 interface IProps {
     score: number;
 }
@@ -12,31 +12,44 @@ interface IState {
 }
 
 export class GameOverPopUp extends Component<IProps, IState> {
-    basicData: { columns: string[]; rows: string[][]; };
+    data: { buyer: string; seller: string; security: string; qty: number; price: number }[];
+    columns: { title: string; dataIndex: string; key: string; width: number; }[];
     constructor(props: any, context: any) {
         super(props, context);
         this.state = {
             username: "",
         };
-        this.basicData = {
-            columns: ['Name', 'Position', 'Office', 'Age', 'Start date', 'Salary'],
-            rows: [
-                ['Tiger Nixon', 'System Architect', 'Edinburgh', '61', '2011/04/25', '$320,800'],
-                ['Garrett Winters', 'Accountant', 'Tokyo', '63', '2011/07/25', '$170,750'],
-                ['Ashton Cox', 'Junior Technical Author', 'San Francisco', '66', '2009/01/12', '$86,000'],
-                ['Cedric Kelly', 'Senior Javascript Developer', 'Edinburgh', '22', '2012/03/29', '$433,060'],
-                ['Airi Satou', 'Accountant', 'Tokyo', '33', '2008/11/28', '$162,700'],
-                ['Brielle Williamson', 'Integration Specialist', 'New York', '61', '2012/12/02', '$372,000'],
-                ['Herrod Chandler', 'Sales Assistant', 'San Francisco', '59', '2012/08/06', '$137,500'],
-                ['Rhona Davidson', 'Integration Specialist', 'Tokyo', '55', '2010/10/14', '$327,900'],
-                ['Colleen Hurst', 'Javascript Developer', 'San Francisco', '39', '2009/09/15', '$205,500'],
-                ['Sonya Frost', 'Software Engineer', 'Edinburgh', '23', '2008/12/13', '$103,600'],
-                ['Jena Gaines', 'Office Manager', 'London', '30', '2008/12/19', '$90,560'],
-                ['Quinn Flynn', 'Support Lead', 'Edinburgh', '22', '2013/03/03', '$342,000'],
-                ['Charde Marshall', 'Regional Director', 'San Francisco', '36', '2008/10/16', '$470,600'],
-                ['Haley Kennedy', 'Senior Marketing Designer', 'London', '43', '2012/12/18', '$313,500'],
-            ],
-        };
+        this.columns = [
+            {
+                title: 'Name',
+                dataIndex: 'name',
+                key: 'name',
+                width: 100,
+            },
+            {
+                title: 'Age',
+                dataIndex: 'age',
+                key: 'age',
+                width: 100,
+            },
+            {
+                title: 'Address',
+                dataIndex: 'address',
+                key: 'address',
+                width: 100,
+            },
+            {
+                title: 'Operations',
+                dataIndex: '',
+                key: 'operations',
+                width: 100,
+            },
+        ];
+
+        this.data = [
+            { buyer: 'John Brown', seller: 'Jim Green', security: 'Apple', qty: 32, price: 100 },
+            { buyer: 'John', seller: 'Jim', security: 'Ap', qty: 3, price: 10 },
+        ];
     }
 
     onSubmit(score: number, username: string) {
@@ -72,7 +85,7 @@ export class GameOverPopUp extends Component<IProps, IState> {
                         onChange={(evt) => this.updateInputValue(evt)}
                     />
 
-                    <MDBTable data={this.basicData} />
+                    <Table columns={this.columns} data={this.data} />
 
                     <div className="returnHomeButton">
                         <Link to="/" className="scoreSubmitButton">
