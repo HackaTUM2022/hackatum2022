e3c716f7b94bac5bb74f953f61df4e9bd124de73
Hamudi Naanaa <hamudi.naanaa@tum.de>
2022-11-19 16:08:08 +0100

    Add Box-Muller transformation for multimodal gaussian distribution generation based on day type (weekday/weekend)

--- server/src/controllers/consumption.controller.js
+++ server/src/controllers/consumption.controller.js
@@ -1,5 +1,5 @@
 const generateDailyConsumption = (req, res) => {
-    const generateDistribution = (nMeans) => {
+    const generateCustomDistribution = (nMeans) => {
         let MIN = 0, MAX = 24;
 
         const randn_bm = (skew) => {
@@ -22,8 +22,9 @@ const generateDailyConsumption = (req, res) => {
 
         let buckets = new Array(24).fill(0);
 
-        // Generate a random cummulative distribution of nMeans Gaussian distributions
+        // Generate a random cummulative distribution of nMeans Gaussian
         for (let i = 0; i < 100000; i++) {
+            // TODO: generate nMeans peaks
             buckets[Math.floor(randn_bm(3))] += 1;
             buckets[Math.floor(randn_bm(1))] += 1;
             buckets[Math.floor(randn_bm(0.25))] += 1;
@@ -36,16 +37,79 @@ const generateDailyConsumption = (req, res) => {
         // enumerate the buckets
         let bucketsEnumerated = [];
         for (let i = 0; i < buckets.length; i++) {
-            bucketsEnumerated.push({
-                hour: i,
-                value: buckets[i],
-            });
+            bucketsEnumerated.push([i, buckets[i]]);
         }
 
         return bucketsEnumerated;
     };
 
-    res.status(200).send(generateDistribution(3));
+    const generateConsumptionDistribution = (baseDistribution) => {
+
+        // add some random noise
+        for (let i = 0; i < baseDistribution.length; i++) {
+            baseDistribution[i][1] += Math.random() * 0.1 - 0.05;
+        }
+
+        return baseDistribution;
+    };
+
+    // User's consumption profile for a weekday (based on the average consumption in Germany (c) Statistisches Bundesamt)
+    let weekdayBaseDistribution = [
+        [0, 0.4],
+        [1, 0.25],
+        [2, 0.15],
+        [3, 0.1],
+        [4, 0.1],
+        [5, 0.13],
+        [6, 0.25],
+        [7, 0.62],
+        [8, 0.67],
+        [9, 0.63],
+        [10, 0.6],
+        [11, 0.55],
+        [12, 0.62],
+        [13, 0.67],
+        [14, 0.6],
+        [15, 0.55],
+        [16, 0.48],
+        [17, 0.5],
+        [18, 0.7],
+        [19, 0.8],
+        [20, 0.85],
+        [21, 0.7],
+        [22, 0.6],
+        [23, 0.47],
+    ]
+
+    // User's consumption profile for a weekend (based on the average consumption in Germany (c) Statistisches Bundesamt)
+    let weekendBaseDistribution = [
+        [0, 0.5],
+        [1, 0.3],
+        [2, 0.25],
+        [3, 0.1],
+        [4, 0.1],
+        [5, 0.13],
+        [6, 0.2],
+        [7, 0.25],
+        [8, 0.3],
+        [9, 0.5],
+        [10, 0.7],
+        [11, 0.82],
+        [12, 0.99],
+        [13, 0.84],
+        [14, 0.71],
+        [15, 0.6],
+        [16, 0.48],
+        [17, 0.5],
+        [18, 0.7],
+        [19, 0.8],
+        [20, 0.85],
+        [21, 0.7],
+        [22, 0.6],
+        [23, 0.47],
+    ]
+
+    res.status(200).send(generateConsumptionDistribution(weekendBaseDistribution));
 }
 
 export {
