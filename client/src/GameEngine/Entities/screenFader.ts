919b7cfbe42836a713e59c660c573c24cc0b222b
Masha <masha@mashahost.localdomain>
2022-11-20 04:07:18 +0100

    Add screen fader

---
+++ client/src/GameEngine/Entities/screenFader.ts
@@ -0,0 +1,31 @@
+import { Entity } from "./entity";
+import { Display } from "../display";
+import { v4 as uuid } from "uuid";
+import { Time } from "../time";
+
+export class ScreenFader implements Entity {
+    x: number = 0;
+    y: number = 0;
+    width: number;
+    height: number;
+    time: Time;
+    color: string;
+    alpha: number;
+    id = uuid();
+
+    constructor(width: number, height: number, time: Time, color: string, alpha: number) {
+        this.width = width;
+        this.height = height;
+        this.time = time;
+        this.color = color;
+        this.alpha = alpha;
+    }
+
+    update(dt: number): void {}
+
+    render(display: Display): void {
+        this.width = display.getXPositionFromTime(this.time.getCurrentTimeInPercentOfDay() * 24);
+
+        display.drawRectangle(this.x, this.y, this.width, this.height, this.color, this.alpha);
+    }
+}
